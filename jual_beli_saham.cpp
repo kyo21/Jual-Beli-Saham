876f8c80a7578adf4e5db2d87ea8339d07555eb0
IKadekEdhoTeguhSentosa <kadekedo49@gmail.com>
2024-01-18 10:28:20 +0800

    menambahkan include vector, mengganti void menjadi int, menambah kodingan dan perulangan, menmbahkan else dan return
    nama : I Kadek Edho Teguh Sentosa
    nim : 2313231046

--- jual_beli_saham.cpp
+++ jual_beli_saham.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
+#include <vector>
 #include <map>
 #include <set>
 #include <string>
 
-void saham_pilihan(){
+int maxProfit(std::vector<int>& harga) {
+    int max_profit = 0;
+    int min_price = harga[0];
 
+    for (int i = 1; i < harga.size(); i++) {
+        if (harga[i] > min_price) {
+            max_profit = std::max(max_profit, harga[i] - min_price);
+        } else {
+            min_price = harga[i];
+        }
+    }
+
+    return max_profit;
 }
 
 
